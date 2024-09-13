e5e714b4d4bd70dfdfabad71770cb709905a59cb
bsucs <122378640+bsucs@users.noreply.github.com>
2024-09-13 16:30:36 -0400

    intial

---
+++ test/app/page.tsx
@@ -0,0 +1,14 @@
+
+
+export default function Home() {
+  return (
+    <ul>
+  <li>butter</li>
+  <li>Eggs</li>
+  <li>Vanilla extract</li>
+  <li>Chocolate Chips</li>
+  <li>Flour</li>
+</ul>
+   
+  );
+}
